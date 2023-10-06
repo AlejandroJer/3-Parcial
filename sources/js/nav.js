42ffa1fd793af8f04f3d9ca071951ed37cfdaf52
ALEJANDRO JERONIMO AZAMAR <ajeronimo@ucol.mx>
2023-10-06 16:14:17 -0600

    The nav in the dashboard is concealable

--- sources/js/nav.js
+++ sources/js/nav.js
@@ -15,3 +15,16 @@ index.forEach((item) => {
         }
     })
 })
+
+const nav = document.querySelector('.navHome')
+const navButton = document.querySelector('#NavArrow')
+
+navButton.addEventListener('click', () => {
+    if(nav.classList.contains('hidden') && navButton.classList.contains('target')){
+        nav.classList.remove('hidden')
+        navButton.classList.remove('target')
+    }else{
+        nav.classList.add('hidden')
+        navButton.classList.add('target')
+    }
+})
