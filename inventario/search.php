7b2d917b8caacf93514642ff2ff47db84bde081d
JDAA4 <116245157+JDAA4@users.noreply.github.com>
2023-10-09 21:39:01 -0600

    Pequeñas correciones

--- inventario/search.php
+++ inventario/search.php
@@ -15,7 +15,7 @@
             </header>
 
             <div class="navHome_ol">
-                <a href="../dashboard.html">
+                <a href="../dashboard.php">
                     <div  class="option_container">
                         <h3>Dashboard</h3>
                     </div>
@@ -31,7 +31,7 @@
                     <ul class="navHome_ul">
                         <a href="./add.php"><li>Agregar Inventario</li></a>
                         <a href="./read.php"><li>Ver Inventario</li></a>
-                        <a href="./search.html"><li class="target">Buscar en Inventario</li></a>
+                        <a href="./search.php"><li class="target">Buscar en Inventario</li></a>
                     </ul>
                 </div>
                 <div class="option">
@@ -43,9 +43,9 @@
                         <span class="arrow"></span>
                     </div>
                     <ul class="navHome_ul hidden">
-                        <a href="../proveedores/add.html"><li>Agregar Proveedor</li></a>
-                        <a href="../proveedores/read.html"><li>Ver Proveedores</li></a>
-                        <a href="../proveedores/search.html"><li>Buscar en Proveedores</li></a>
+                        <a href="../proveedores/add.php"><li>Agregar Proveedor</li></a>
+                        <a href="../proveedores/read.php"><li>Ver Proveedores</li></a>
+                        <a href="../proveedores/search.php"><li>Buscar en Proveedores</li></a>
                     </ul>
                 </div>
                 <div class="option">
@@ -58,8 +58,8 @@
                     </div>
                     <ul class="navHome_ul hidden">
                         <a href="../empleados/add.php"><li>Agregar Empleado</li></a>
-                        <a href="../empleados/read.html"><li>Ver Empleados</li></a>
-                        <a href="../empleados/search.html"><li>Buscar en Empleados</li></a>
+                        <a href="../empleados/read.php"><li>Ver Empleados</li></a>
+                        <a href="../empleados/search.php"><li>Buscar en Empleados</li></a>
                     </ul>
                 </div>
             </div>
