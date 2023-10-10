faaf5541b97695e3e856123e224c75e8c55dc450
rcasillas2 <rcasillas2@ucol.mx>
2023-10-09 19:54:56 -0600

    Add, read de proveedores y css de button en read

---
+++ preveedores/add_proveedor.php
@@ -0,0 +1,34 @@
+<?php
+    // Conexión BD
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "jemas";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Conexión fallida: " . $conn->connect_error);
+    }
+
+    // Recibir los datos y almacenarlos en variables
+    $idProveedor = $_POST["id-proveedor"];
+    $nombreEmpresa = $_POST["nombre-empresa"];
+    $direccionProveedor = $_POST["direccion-proveedor"];
+    $personaContacto = $_POST["persona-contacto"];
+    $telefono = $_POST["telefono"];
+    $correoProveedor = $_POST["correo-proveedor"];
+
+    // Insertar datos en la BD
+    $sql = "INSERT INTO proveedores (id_proveedor, nombre_empresa, direccion, persona_contacto, num_telefono, email_proveedor)
+    VALUES ('$idProveedor', '$nombreEmpresa', '$direccionProveedor', '$personaContacto', '$telefono', '$correoProveedor')";
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "Datos guardados correctamente";
+        echo '<a href="../dashboard.html" class="btn btn-primary">Volver al Dashboard</a>';
+    } else {
+        echo "Error al guardar los datos: " . $conn->error;
+    }
+    
+    $conn->close();
+?>
---
+++ preveedores/read_proveedores.php
@@ -0,0 +1,41 @@
+<?php
+    //Conexión 
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "jemas";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Conexión fallida: " . $conn->connect_error);
+    }
+
+    //Consulta
+    $sql = "SELECT * FROM proveedores";
+    $result = $conn->query($sql);
+
+    //Verificacion de datos y tabla
+    if ($result->num_rows > 0) {
+        echo "<h2>Lista de Proveedores</h2>";
+        echo "<table>";
+        echo "<tr><th>ID del proveedor</th><th>Nombre de la Empresa</th><th>Dirección</th><th>Persona de Contacto</th><th>Teléfono</th><th>Correo Electrónico</th></tr>";
+
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row["id_proveedor"] . "</td>";
+            echo "<td>" . $row["nombre_empresa"] . "</td>";
+            echo "<td>" . $row["direccion"] . "</td>";
+            echo "<td>" . $row["persona_contacto"] . "</td>";
+            echo "<td>" . $row["num_telefono"] . "</td>";
+            echo "<td>" . $row["email_proveedor"] . "</td>";
+            echo "</tr>";
+        }
+
+        echo "</table>";
+    } else {
+        echo "No hay proveedores registrados.";
+    }
+
+    $conn->close();
+?>
