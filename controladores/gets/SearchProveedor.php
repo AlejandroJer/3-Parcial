a06e6060d6964d2237803f28fc3f167310c1f4f6
rcasillas2 <rcasillas2@ucol.mx>
2023-10-15 16:26:51 -0600

    Holis

---
+++ controladores/gets/SearchProveedor.php
@@ -0,0 +1,46 @@
+<?php
+    namespace controladores;
+    require_once("./../../autoload.php");
+    use modelos\proveedores; 
+    $proveedor = new proveedores();
+
+    $limit = 2;
+
+    if (!isset($_POST['search']) && !isset($_POST['submit'])) {
+        header("location:./../../proveedores/search.php");
+    } else {
+        $keyword = filter_var($_POST['search'], FILTER_SANITIZE_STRING);
+
+        if (!$_POST['search'] || $_POST['search'] == " ") {
+            header("location:./../../proveedores/search.php");
+            die();
+        }
+
+        $index = $proveedor->GetProveedorByKeywordIndex($keyword); 
+        $index = ceil($index / $limit);
+        $results = $proveedor->GetProveedorByKeywordLimited($keyword, 0, $limit); 
+
+        $_SESSION['results'] = $results;
+        $_SESSION['keyword'] = $keyword;
+        $_SESSION['index'] = $index;
+        $_SESSION['pageClicked'] = 0;
+        header("location:./../../proveedores/search.php");
+    }
+
+    if (!isset($_POST['submitPaginated'])) {
+        header("location:./../../proveedores/search.php");
+    } else {
+        $keyword = filter_var($_POST['searchPaginated'], FILTER_SANITIZE_STRING);
+        $page = filter_var($_POST['submitPaginated'], FILTER_SANITIZE_NUMBER_INT);
+
+        $index = $proveedor->GetProveedorByKeywordIndex($keyword); 
+        $index = ceil($index / $limit);
+        $results = $proveedor->GetProveedorByKeywordLimited($keyword, $page * $limit, $limit);
+
+        $_SESSION['results'] = $results;
+        $_SESSION['keyword'] = $keyword;
+        $_SESSION['index'] = $index;
+        $_SESSION['pageClicked'] = $page;
+        header("location:./../../proveedores/search.php");
+    }
+?>
